c51056a66bc120f70279e95271610d76157a255c
OneTwo7 <lahoohal@gmail.com>
2024-02-19 17:45:38 +0400

    db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,35 @@
+import { config as dotEnvConfig } from 'dotenv-safe';
 import express from 'express';
+import { DataSource } from 'typeorm';
+
+dotEnvConfig({
+  example: '.env',
+});
 
 const PORT = 4000;
 const app = express();
 
-app.get('/', (_, res) => {
-  res.send('Hello there');
-});
+async function main() {
+  const dataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  await dataSource.initialize();
+
+  app.get('/', (_, res) => {
+    res.send('Hello there');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+main().catch(err => {
+  console.error(err);
 });
